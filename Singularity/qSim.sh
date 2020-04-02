ef0820a9a8a6a190aed1e9106b8ff797e02a59f8
Yadav Dosieah <dosyad12@gmail.com>
2020-04-02 15:07:43 +0100

    Auto stash before merge of "master" and "origin/master"

--- Singularity/qSim.sh
+++ Singularity/qSim.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #$ -P acsehpc
 #$ -q acsehpc.q
-#$ -l rmem=8G
+#$ -l rmem=4G
 #$ -l h_rt=48:00:00
-#$ -pe smp 20
+#$ -pe smp 10
 #$ -m bea
 #$ -M gydosieah1@sheffield.ac.uk
 #$ -o output.txt
 #$ -e error.txt
 #$ -j y
-# 8GB memory, 48hrs max, 20 processors
+# 8GB memory, 48hrs max, 10 processors
 
 #Have Singularity call a custom script from your home or other mounted directories
 #Don't forget to make the script executable before running by using chmod
--- enki/examples/playground/Set_Up.py
+++ enki/examples/playground/Set_Up.py
@@ -5,7 +5,8 @@ import csv
 import matplotlib.pyplot as plt
 import random
 
-noOfSheep = 10
+noOfObjects = 7
+noOfSheep = 7
 noOfShepherd = 3
 fig = plt.figure(figsize=(8, 10))
 ax = plt.axes(xlim=(-20, 320), ylim=(-20, 400))
@@ -13,6 +14,7 @@ plt.axis('off')
 
 sheepDots = {}
 shepherdDot = {}
+ObjectDot = {}
 
 rec2 = plt.Rectangle((0,0),300,300,fill=True,edgecolor = None, color='#d3d3d3')
 plt.annotate(s='', xy=(0,-10), xytext=(300,-10), arrowprops=dict(arrowstyle='<->'))
@@ -31,8 +33,8 @@ ax.add_patch(rec1)
 plt.annotate(s='25cm', xy=(150,250), xytext=(175,267.7), arrowprops=dict(arrowstyle='<-'))
 plt.annotate(s='50cm', xy=(150,250), xytext=(200,215.6), arrowprops=dict(arrowstyle='<-'))
 
-plt.annotate(s='Goal Area', xy=(150,290), xytext=(200,315), arrowprops=dict(arrowstyle='<-',color='b'),color='b')
-plt.annotate(s='Initial Area for Sheep and Shepherds', xy=(10,190), xytext=(10,345), arrowprops=dict(arrowstyle='<-',color='g'),color='g')
+plt.annotate(s='Goal Area', xy=(150,290), xytext=(190,315), arrowprops=dict(arrowstyle='<-',color='b'),color='b')
+plt.annotate(s='Initial Area for Sheep, Shepherds and Objects', xy=(10,190), xytext=(10,345), arrowprops=dict(arrowstyle='<-',color='g'),color='g')
 goal = plt.Circle((150,250),25,fc='b')
 goal2 = plt.Circle((150,250),50,ls=':',fill=False)
 ax.add_patch(goal)
@@ -43,18 +45,25 @@ for i in range(noOfSheep):
     ax.add_patch(sheepDots[i])
 
 for n in range(noOfShepherd):
-    shepherdDot[n] = plt.Circle((random.randint(0,300),random.randint(0,300)),3.7,fc='r')
+    shepherdDot[n] = plt.Circle((random.randint(0,300),random.randint(0,200)),3.7,fc='r')
     ax.add_patch(shepherdDot[n])
 
-legendbox = plt.Rectangle((240,340),60,60,fill=False,edgecolor='k')
+for m in range(noOfObjects):
+    ObjectDot[m] = plt.Circle((random.randint(0,300),random.randint(0,200)),3.7,fc='gold')
+    ax.add_patch(ObjectDot[m])
+
+legendbox = plt.Rectangle((240,320),60,60,fill=False,edgecolor='k')
 legend1 = plt.Circle((250, 350),3.7,fc='g')
 legend2 = plt.Circle((250, 370),3.7,fc='r')
+legend3 = plt.Circle((250, 330),3.7,fc='gold')
 ax.add_patch(legend1)
 ax.add_patch(legend2)
+ax.add_patch(legend3)
 ax.add_patch(legendbox)
 ax.annotate('Legend', xy=(270,390), ha='center', va='center', rotation=0)
 ax.annotate('Sheep', xy=(260,350), ha='left', va='center', rotation=0)
 ax.annotate('Shepherd', xy=(260,370), ha='left', va='center', rotation=0)
+ax.annotate('Object', xy=(260,330), ha='left', va='center', rotation=0)
 #
 # def init():
 #     for i in range(noOfSheep):
