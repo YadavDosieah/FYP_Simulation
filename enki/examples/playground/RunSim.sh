fb36e1558a416a339a5370760263998171cc54cc
= <dosyad12@gmail.com>
2020-04-02 18:59:25 +0100

    Added Analysis code + cxx_flags

--- enki/examples/playground/RunSim.sh
+++ enki/examples/playground/RunSim.sh
@@ -8,17 +8,28 @@ make enkiplayground
 
 sed -i '/^No_Of_Threads		=/s/=.*/= 10;/' Parameters.cfg
 
-sed -i '/^mode	       	=/s/=.*/= 0;/' Parameters.cfg
-sed -i '/^noOfSheep		  =/s/=.*/= 10;/' Parameters.cfg
-sed -i '/^noOfObjects		=/s/=.*/= 0;/' Parameters.cfg
-./enkiplayground
+if [ $1 -eq 0 ]
+then
+  sed -i '/^mode	       	=/s/=.*/= 0;/' Parameters.cfg
+  sed -i '/^noOfSheep		  =/s/=.*/= 10;/' Parameters.cfg
+  sed -i '/^noOfObjects		=/s/=.*/= 0;/' Parameters.cfg
+  echo "Shepherding"
+  ./enkiplayground
 
-# sed -i '/^mode		      =/s/=.*/= 1;/' Parameters.cfg
-# sed -i '/^noOfSheep		  =/s/=.*/= 0;/' Parameters.cfg
-# sed -i '/^noOfObjects		=/s/=.*/= 10;/' Parameters.cfg
-# ./enkiplayground
-#
-# sed -i '/^mode		      =/s/=.*/= 2;/' Parameters.cfg
-# sed -i '/^noOfSheep	  	=/s/=.*/= 10;/' Parameters.cfg
-# sed -i '/^noOfObjects		=/s/=.*/= 10;/' Parameters.cfg
-# ./enkiplayground
+elif  [ $1 -eq 1 ]
+then
+  sed -i '/^mode		      =/s/=.*/= 1;/' Parameters.cfg
+  sed -i '/^noOfSheep		  =/s/=.*/= 0;/' Parameters.cfg
+  sed -i '/^noOfObjects		=/s/=.*/= 10;/' Parameters.cfg
+  echo "Object Clustering"
+  ./enkiplayground
+
+elif  [ $1 -eq 2 ]
+then
+  sed -i '/^mode		      =/s/=.*/= 2;/' Parameters.cfg
+  sed -i '/^noOfSheep	  	=/s/=.*/= 10;/' Parameters.cfg
+  sed -i '/^noOfObjects		=/s/=.*/= 10;/' Parameters.cfg
+  echo "Shepherding + Object Clustering"
+  ./enkiplayground
+
+fi
