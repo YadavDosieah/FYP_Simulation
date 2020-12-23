22a2a4cfbb5fd0caf2deac5341cc55303b8d2c8d
Gopesh Dosieah <co4xgd@sharc-login1.shef.ac.uk>
2020-12-23 10:57:38 +0000

    Simplified Controller re-run results 1

--- Singularity/qSim.sh
+++ Singularity/qSim.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#$ -P acsehpc
-#$ -q acsehpc.q
+##$ -P acsehpc
+##$ -q acsehpc.q
 #$ -l rmem=1G
-#$ -l h_rt=27:00:00
+#$ -l h_rt=24:30:00
 #$ -pe smp 10
 #$ -m bea
 #$ -M co4xgd@sheffield.ac.uk
