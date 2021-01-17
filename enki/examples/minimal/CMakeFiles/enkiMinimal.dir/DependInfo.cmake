5192adcf23e8ed997907df090087b3304f9331e3
Yadav Dosieah <dosyad12@gmail.com>
2021-01-17 23:21:49 +0400

    Debugging?

--- enki/examples/minimal/CMakeFiles/enkiMinimal.dir/DependInfo.cmake
+++ enki/examples/minimal/CMakeFiles/enkiMinimal.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/co4xgd/FYP_Simulation/enki/examples/minimal/enkiMinimal.cpp" "/home/co4xgd/FYP_Simulation/enki/examples/minimal/CMakeFiles/enkiMinimal.dir/enkiMinimal.cpp.o"
+  "/home/yadav/Documents/Final-Year-Project/FYP_Simulation/enki/examples/minimal/enkiMinimal.cpp" "/home/yadav/Documents/Final-Year-Project/FYP_Simulation/enki/examples/minimal/CMakeFiles/enkiMinimal.dir/enkiMinimal.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
@@ -15,7 +15,7 @@ set(CMAKE_CXX_TARGET_INCLUDE_PATH
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
-  "/home/co4xgd/FYP_Simulation/enki/enki/CMakeFiles/enki.dir/DependInfo.cmake"
+  "/home/yadav/Documents/Final-Year-Project/FYP_Simulation/enki/enki/CMakeFiles/enki.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
--- enki/python/cmake_install.cmake
+++ enki/python/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/co4xgd/FYP_Simulation/enki/python
+# Install script for directory: /home/yadav/Documents/Final-Year-Project/FYP_Simulation/enki/python
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -38,25 +38,25 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
 endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
-  if(EXISTS "$ENV{DESTDIR}/usr/local/lib/python2.7/dist-packages/pyenki.so" AND
-     NOT IS_SYMLINK "$ENV{DESTDIR}/usr/local/lib/python2.7/dist-packages/pyenki.so")
+  if(EXISTS "$ENV{DESTDIR}/usr/local/lib/python3/dist-packages/pyenki.so" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}/usr/local/lib/python3/dist-packages/pyenki.so")
     file(RPATH_CHECK
-         FILE "$ENV{DESTDIR}/usr/local/lib/python2.7/dist-packages/pyenki.so"
+         FILE "$ENV{DESTDIR}/usr/local/lib/python3/dist-packages/pyenki.so"
          RPATH "")
   endif()
   list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
-   "/usr/local/lib/python2.7/dist-packages/pyenki.so")
+   "/usr/local/lib/python3/dist-packages/pyenki.so")
   if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
   if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
-file(INSTALL DESTINATION "/usr/local/lib/python2.7/dist-packages" TYPE MODULE FILES "/home/co4xgd/FYP_Simulation/enki/python/pyenki.so")
-  if(EXISTS "$ENV{DESTDIR}/usr/local/lib/python2.7/dist-packages/pyenki.so" AND
-     NOT IS_SYMLINK "$ENV{DESTDIR}/usr/local/lib/python2.7/dist-packages/pyenki.so")
+file(INSTALL DESTINATION "/usr/local/lib/python3/dist-packages" TYPE MODULE FILES "/home/yadav/Documents/Final-Year-Project/FYP_Simulation/enki/python/pyenki.so")
+  if(EXISTS "$ENV{DESTDIR}/usr/local/lib/python3/dist-packages/pyenki.so" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}/usr/local/lib/python3/dist-packages/pyenki.so")
     if(CMAKE_INSTALL_DO_STRIP)
-      execute_process(COMMAND "/usr/bin/strip" "$ENV{DESTDIR}/usr/local/lib/python2.7/dist-packages/pyenki.so")
+      execute_process(COMMAND "/usr/bin/strip" "$ENV{DESTDIR}/usr/local/lib/python3/dist-packages/pyenki.so")
     endif()
   endif()
 endif()
