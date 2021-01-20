3f30e493ac20112d37d969ce2b8f809d063200f2
Yadav Dosieah <dosyad12@gmail.com>
2021-01-20 13:23:13 +0400

    Preps for post eval on sharc

--- Singularity/qSim.sh
+++ Singularity/qSim.sh
@@ -2,8 +2,8 @@
 #$ -P acsehpc
 #$ -q acsehpc.q
 #$ -l rmem=1G
-#$ -l h_rt=04:00:00
-#$ -pe smp 10
+#$ -l h_rt=03:00:00
+#$ -pe smp 20
 #$ -m bea
 #$ -M co4xgd@sheffield.ac.uk
 #$ -e error.txt
@@ -13,5 +13,8 @@
 #Have Singularity call a custom script from your home or other mounted directories
 #Don't forget to make the script executable before running by using chmod
 
-chmod +x ../enki/examples/playground/RunOptimisation.sh
-singularity exec myimage.simg ../enki/examples/playground/RunOptimisation.sh $1
+# chmod +x ../enki/examples/playground/RunOptimisation.sh
+# singularity exec myimage.simg ../enki/examples/playground/RunOptimisation.sh $1
+
+chmod +x ../enki/examples/playground/Auto_PostEval.sh
+singularity exec myimage.simg ../enki/examples/playground/Auto_PostEval.sh
